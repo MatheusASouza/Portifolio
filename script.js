dd734204a1f87fbd83d936b476f4e305728f4738
Matheus Alves de Souza <79257255+MatheusASouza@users.noreply.github.com>
2021-02-25 18:28:46 -0300

    Menu concluído

--- script.js
+++
@@ -1,5 +0,0 @@
-var hamburguer = document.querySelector(".hamburguer");
-
-hamburguer.addEventListener("cick", function(){
-   document.querySelector(".container").classList.toggle(".show-menu");
-});   
