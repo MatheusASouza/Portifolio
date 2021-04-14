16ab5462ce6f176d9169656371b00ec9a5746779
Matheus Alves de Souza <79257255+MatheusASouza@users.noreply.github.com>
2021-04-13 23:58:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document.querySelector("#itens-carrossel")
-.addEventListener("wheel", event=>{
+const nextButton = document.querySelector("#itens-carrossel")
+.addEventListener("click", event=>{
     if(event.deltaY > 0){
         event.target.scrollBy(300,0)
     } else{
